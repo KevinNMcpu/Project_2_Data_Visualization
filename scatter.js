d9d1185ffc404658bc2454affb4226e11c96dbd6
SarahZ <szachrich@yahoo.com>
2020-10-21 14:47:09 -0600

    add working site html w/ organization

--- scatter.js
+++ scatter.js
@@ -1,6 +1,6 @@
 //creating the scatter plot data for numbers of players vs metascore
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
+var margin = {top:20, right: 60, bottom: 80, left: 80},
     width = 900 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
     // append the svg object to the body of the page
@@ -8,17 +8,19 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
       .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+
       .append("g")
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")")
 
 //Read the data
-d3.csv('new_steam_db1.csv', function(data) {
+d3.csv('Assets/Data/new_steam_db1.csv', function(data) {
   console.log(data)
   // Add X axis
   var x = d3.scaleLinear()
     .domain([0, 0])
     .range([ 0, width ]);
+
   svg.append("g")
     .attr("class", "myXaxis")  
     .attr("transform", "translate(0," + height + ")")
@@ -41,7 +43,7 @@ d3.csv('new_steam_db1.csv', function(data) {
       .attr("cx", function (d) { return x(d.Owners); } )
       .attr("cy", function (d) { return y(d.Metascores); } )
       .attr("r", 5)
-      .style("fill", "red");
+      .style("fill", "#800000");
 
   // new X axis
   x.domain([-1000000000, 10000000000])
@@ -61,14 +63,14 @@ d3.csv('new_steam_db1.csv', function(data) {
   //Create Axes Labels
   svg.append('text')
      .attr('transform', "rotate(-90)")
-     .attr('y', 0-margin.left)
+     .attr('y', 10-margin.left)
      .attr('x', 0-(height/2))
      .attr('dy', "1em")
      .attr('class', 'axisText')
      .text("Metascore")
 
     svg.append('text')
-     .attr('transform', `translate(${width/2}, ${height+27})`)
+     .attr('transform', `translate(${width/2}, ${height+50})`)
      .attr('class','axisText')
      .text('Number of Players')
 })
